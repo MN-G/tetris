2951ffefe3858638ef3d154e59659b013389b2a9
MN-G <michaelngabriel@gmail.com>
2024-05-22 13:54:11 -0700

    Add direction and rotation functions

--- tetris/lib/tetris/tetromino.ex
+++ tetris/lib/tetris/tetromino.ex
@@ -1,4 +1,5 @@
 defmodule Tetris.Tetromino do
+  alias Tetris.Point
   defstruct shape: :l, rotation: 0, location: {5, 1}
 
   def new(options \\ []) do
@@ -13,4 +14,23 @@ defmodule Tetris.Tetromino do
     ~w(i t o l j z s)a
     |> Enum.random()
   end
+
+  def right(tetro) do
+    %{tetro | location: Point.right(tetro.location)}
+  end
+
+  def left(tetro) do
+    %{tetro | location: Point.left(tetro.location)}
+  end
+
+  def down(tetro) do
+    %{tetro | location: Point.down(tetro.location)}
+  end
+
+  def rotate(tetro) do
+    %{tetro | rotation: rotate_degrees(tetro.rotation)}
+  end
+
+  defp rotate_degrees(270), do: 0
+  defp rotate_degrees(d), do: d + 90
 end
