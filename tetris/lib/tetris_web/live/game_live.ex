9404ed9d5e308b190d158cf1aa50f7d1940e4c1b
MN-G <michaelngabriel@gmail.com>
2024-05-22 17:37:24 -0700

    Add starting tetromino, ticker and handle info

--- tetris/lib/tetris_web/live/game_live.ex
+++ tetris/lib/tetris_web/live/game_live.ex
@@ -1,13 +1,29 @@
 defmodule TetrisWeb.GameLive do
+  alias Tetris.Tetromino
   use TetrisWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    :timer.send_interval(500, :tick)
+    {:ok, socket |> new_tetromino}
+  end
+
+  def new_tetromino(socket) do
+    assign(socket, tetro: Tetromino.new_random())
   end
 
   def render(assigns) do
     ~H"""
-    <div>Hello Tetris</div>
+    <div>
+      Hello Tetris <pre> <%= inspect @tetro %> </pre>
+    </div>
     """
+  end
+
+  def down(%{assigns: %{tetro: tetro}} = socket) do
+    assign(socket, tetro: Tetromino.down(tetro))
+  end
+
+  def handle_info(:tick, socket) do
+    {:noreply, down(socket)}
   end
 end
