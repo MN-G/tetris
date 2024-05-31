436ae08176b20c6dd27387ff505c832d3d21aa5a
MN-G <michaelngabriel@gmail.com>
2024-05-30 20:03:20 -0700

    add colors to tetrominos

--- tetris/lib/tetris_web/live/game_live.ex
+++ tetris/lib/tetris_web/live/game_live.ex
@@ -3,7 +3,10 @@ defmodule TetrisWeb.GameLive do
   use TetrisWeb, :live_view
 
   def mount(_params, _session, socket) do
+    if connected?(socket) do
     :timer.send_interval(500, :tick)
+      end
+
     {:ok, socket |> new_tetromino |> display}
   end
 
@@ -12,7 +15,9 @@ defmodule TetrisWeb.GameLive do
     <div>
       <h1>Welcome to Tetris</h1>
        <%= render_board(assigns) %>
+      <pre>
       <%= inspect @tetro %> 
+      </pre>
     </div>
     """
   end
@@ -23,7 +28,7 @@ defmodule TetrisWeb.GameLive do
       <svg width="200" height="400">
         <rect height="400" width="200" style="fill:rgb=(0,0,0);" />
         <%= for {x,y} <- @points do %>
-          <.render_points x={x} y={y} />
+          <.render_points x={x} y={y} shape={color(@tetro.shape)} />
         <% end %>
       </svg>
     </div>
@@ -32,13 +37,23 @@ defmodule TetrisWeb.GameLive do
 
   attr :x, :integer, required: true
   attr :y, :integer, required: true
+  attr :shape, :string, required: true
 
   # <rect height="20" width="20" x={(@x - 1) * 20} y={(@y - 1) * 20} style="fill:rgb(255,0,0);" />
   defp render_points(assigns) do
     ~H"""
-    <rect height="20" width="20" x={(@x - 1) * 20} y={(@y - 1) * 20} style="fill:rgb(255,0,0);" />
+    <rect height="20" width="20" x={(@x - 1) * 20} y={(@y - 1) * 20} style={@shape}; />
     """
   end
+
+  defp color(:l), do: "fill:red"
+  defp color(:j), do: "fill:royalblue"
+  defp color(:s), do: "fill:limegreen"
+  defp color(:z), do: "fill:yellow"
+  defp color(:o), do: "fill:magenta"
+  defp color(:i), do: "fill:silver"
+  defp color(:t), do: "fill:saddlebrown"
+  defp color(_), do: "fill:red"
 
   def new_tetromino(socket) do
     assign(socket, tetro: Tetromino.new_random())
